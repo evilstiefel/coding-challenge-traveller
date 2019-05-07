7644ddfeaaf4e0c73a48bc1c66270e9a5c6d2270
Steffen Sachse <steffen.sachse@uni-rostock.de>
2019-05-07 22:23:32 +0200

    Kommentierung Quellcode

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 'use strict';
 import fs from 'fs';
+import { from } from 'rxjs';
+import { pairwise, map, scan, takeLast } from 'rxjs/operators';
+
+/**
+ * Lösung (Ausgabe des Programms in der Konsole):
+ * The shortest path is 7 steps long (not including earth):
+ * Erde -> node_810 -> node_595 -> node_132 -> node_519 -> node_71 -> node_432 -> b3-r7-r4nd7
+ * Calculated cost for the path: 2.995687895999458.
+ */
 
 const file = `${__dirname}/../assets/generatedGraph.json`;
 
@@ -20,6 +29,11 @@ interface IPlanetEdge {
   cost: number;
 }
 
+/**
+ * Liefert ein Array aus der Nummer des Nachbarn und den Kosten für den Weg dorthin
+ * @param startingNode Node, von der aus wir nach Nachbarn suchen
+ * @param edges Alle Kanten des Graphs
+ */
 const getNeighbors = (startingNode: number, edges: IPlanetEdge[]): Array<{ neighborNumber: number, distance: number }> => {
   return edges
     .filter((e) => e.source === startingNode || e.target === startingNode)
@@ -31,15 +45,26 @@ const getNeighbors = (startingNode: number, edges: IPlanetEdge[]): Array<{ neigh
     });
 };
 
-const markNodeVisited = (label: string, mapping: Map<string, INodeDistance>) => {
-  let node = mapping.get(label);
-  node = {
-    ...node,
-    visited: true,
-  };
-  mapping.set(label, node);
+/**
+ * Liefert die Kante, die zwei Knoten miteinander verbindet, sonst undefined
+ * @param node1 Knoten 1
+ * @param node2 Knoten 2
+ * @param edges Alle Kanten des Graphs
+ */
+const findEdge = (node1: number, node2: number, edges: IPlanetEdge[]): IPlanetEdge | undefined => {
+  return edges.find((e) => {
+    return (
+      (e.source === node1 && e.target === node2) ||
+      (e.target === node1 && e.source === node2)
+    );
+  });
 };
 
+/**
+ * Gegeben die Aufgabenstellung wandelt diese Funktion eine Knotennummer
+ * in das zugehörige Label um
+ * @param nodeNumber Nummer des Knotens
+ */
 const numberToLabel = (nodeNumber: number): string => {
   switch (nodeNumber) {
     case 18:
@@ -51,6 +76,11 @@ const numberToLabel = (nodeNumber: number): string => {
   }
 };
 
+/**
+ * Gegeben die Aufgabenstellung wandelt diese Funktion ein Knotenlabel in
+ * die zugehörige Nummer um
+ * @param label Bezeichnung des Knotens
+ */
 const labelToNumber = (label: string): number => {
   switch (label) {
     case 'Erde':
@@ -63,9 +93,13 @@ const labelToNumber = (label: string): number => {
 };
 
 (async () => {
+  /**
+   * Wir lesen die Datei asynchron ein und nutzen das Callback dann zur Berechnung
+   * des Weges.
+   */
   fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
     if (err) {
-      console.error('Could not open graph file for reading');
+      console.error('Konnte die Quelldatei nicht einlesen');
       return;
     }
     const parsedData: { nodes: IPlanetNode[], edges: IPlanetEdge[] } = JSON.parse(data);
@@ -76,7 +110,6 @@ const labelToNumber = (label: string): number => {
       .forEach((node) => nodeInfoMap
         .set(node.label, { node, distance: (node.label === 'Erde') ? 0 : Number.POSITIVE_INFINITY, visited: (node.label === 'Erde') ? true : false, parent: undefined }));
     parsedData.nodes.forEach((node) => {
-      // The node labels are strings, but we need the numbers, so let's fix that
       const numLabel = labelToNumber(node.label);
       neighborMap.set(node.label, getNeighbors(numLabel, parsedData.edges));
     });
@@ -88,18 +121,14 @@ const labelToNumber = (label: string): number => {
     while (currentNode !== undefined) {
       const neighbors = neighborMap.get(currentNode.node.label);
       neighbors.forEach((n) => {
-        const neighbor = {...nodeInfoMap.get(numberToLabel(n.neighborNumber))};
+        const neighbor = { ...nodeInfoMap.get(numberToLabel(n.neighborNumber)) };
         const distance = currentNode.distance + n.distance;
         const isShorter = neighbor.distance > distance;
         if (isShorter) {
-          console.log(`Shorter, setting parent to ${currentNode.node.label}`);
           neighbor.distance = distance;
           neighbor.parent = currentNode.node.label;
         }
         nodeInfoMap.set(neighbor.node.label, neighbor);
-        if (numberToLabel(n.neighborNumber) === 'b3-r7-r4nd7') {
-          console.log('got to target');
-        }
       });
       currentNode.visited = true;
       visited.set(labelToNumber(currentNode.node.label), true);
@@ -113,12 +142,35 @@ const labelToNumber = (label: string): number => {
         currentNode = undefined;
       }
     }
-    console.log('finished');
-    let currentLabel = nodeInfoMap.get('b3-r7-r4nd7').node.label;
 
+    // Ermittlung des Pfades durch "Rückwärtslaufen" der Parent-Bezeichner
+    let currentLabel = nodeInfoMap.get('b3-r7-r4nd7').node.label;
+    let path: string[] = [];
     while (currentLabel !== undefined) {
-      console.log(currentLabel);
+      path.push(currentLabel);
       currentLabel = nodeInfoMap.get(currentLabel).parent;
     }
+    // Natürlich wollen wir nicht rückwärts fliegen, also drehen wir den Weg um
+    path = path.reverse();
+    /**
+     * Jetzt berechnen wir noch einmal aus den paarweisen Elementen der Strecke
+     * die Gesamtkosten. Nicht Teil der Aufgabenstellung, aber meines Debuggings
+     */
+    from(path).pipe(
+      pairwise(),
+      map((pair) => findEdge(labelToNumber(pair[0]), labelToNumber(pair[1]), parsedData.edges)),
+      map((edge) => edge.cost),
+      scan((acc, edge) => acc + edge, 0),
+      takeLast(1),
+    ).subscribe((sum) => {
+      /**
+       * Die Summe vergleichen wir mit der gespeicherten Distanz im Ziel.
+       * Diese müssen identisch sein, wenn der Weg so stimmt. Wird hier
+       * nicht benutzt, könnte man aber.
+       */
+      console.log(`The shortest path is ${path.length - 1} steps long (not including earth): `);
+      console.log(path.join(' -> '));
+      console.log(`Calculated cost for the path: ${sum}.`);
+    });
   });
 })();
