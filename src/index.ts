6d4be980dd8dab055461122c90f203055a24c610
Steffen Sachse <steffen.sachse@uni-rostock.de>
2019-05-07 17:26:51 +0200

    Tslint und interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
+'use strict';
 import fs from 'fs';
-console.log("test");
+
+const file = `${__dirname}/../assets/generatedGraph.json`;
+
+interface IPlanetNode {
+  label: string;
+}
+
+interface IPlanetEdge {
+  source: number;
+  target: number;
+  cost: number;
+}
+
+fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
+  if (err) {
+    console.error('Could not open graph file for reading');
+    return;
+  }
+  const { nodes, edges }: { nodes: IPlanetNode[], edges: IPlanetEdge[] } = JSON.parse(data);
+});
